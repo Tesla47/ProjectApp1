fe73350b29a989629d7acd540dc6a93d651695ce
Tesla47 <sayedshabeerhashimi@example.com>
2021-04-24 14:43:08 +0430

    24.April 2021

---
+++ data/AboutPageImages.tsx
@@ -0,0 +1,40 @@
+import { ImageData } from "./captionsArray";
+
+export var images = [
+  {
+    url: require("../assets/AboutPage/Roien.png"),
+    id: 1,
+    title: ImageData["image1"].title,
+    description: ImageData["image1"].description,
+  },
+  {
+    url: require("../assets/AboutPage/Roien.png"),
+    id: 2,
+    title: ImageData["image1"].title,
+    description: ImageData["image1"].description,
+  },
+  {
+    url: require("../assets/AboutPage/Roien.png"),
+    id: 3,
+    title: ImageData["image1"].title,
+    description: ImageData["image1"].description,
+  },
+  {
+    url: require("../assets/AboutPage/Roien.png"),
+    id: 4,
+    title: ImageData["image1"].title,
+    description: ImageData["image1"].description,
+  },
+  {
+    url: require("../assets/AboutPage/Roien.png"),
+    id: 5,
+    title: ImageData["image1"].title,
+    description: ImageData["image1"].description,
+  },
+  {
+    url: require("../assets/AboutPage/Roien.png"),
+    id: 6,
+    title: ImageData["image1"].title,
+    description: ImageData["image1"].description,
+  },
+];
--- screens/About.tsx
+++ screens/About.tsx
@@ -1,15 +1,67 @@
 import React, { Component, useState } from "react";
-import { StyleSheet, Text, View, Dimensions, Button } from "react-native";
-import Header from "../components/Header";
-import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Dimensions,
+  Button,
+  ProgressViewIOSComponent,
+} from "react-native";
+import { NavigationStackProp } from "react-navigation-stack";
+import { images } from "../data/AboutPageImages";
 import { ScrollView } from "react-native-gesture-handler";
-import Icon from "react-native-vector-icons/FontAwesome";
+import Header from "../components/Header";
+import Icon from "react-native-vector-icons/Ionicons";
 
-export default class About extends Component {
+interface Props {
+  navigation: NavigationStackProp;
+}
+interface State {
+  images: any;
+  id: number
+  
+}
+
+export default class About extends Component<Props, State> {
+  viewY: any = {};
+  scrollView: any;
+  scrolledId: number = 1;
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      images: images,
+      id: 1,
+    };
   }
+
+  static navigationOptions = () => {
+    return {
+      header: null,
+    };
+  };
+
+  async componentDidMount() {}
+
+  scrollTo = () => {
+      setTimeout(async () => {
+        this.scrollView.scrollTo({
+          x: 0,
+          y: this.viewY[this.scrolledId],
+          animated: true,
+        });
+      }, 100);
+    this.scrolledId = this.scrolledId + 1;
+  };
+
+  getAllImages = () => {
+    var arr = [];
+    this.state.images.forEach((element) => {
+      var image = this.loadImage(element);
+      arr.push(image);
+    });
+    return arr;
+  };
 
   render() {
     return (
@@ -17,35 +69,87 @@ export default class About extends Component {
         <View>
           <Header />
         </View>
-        <View style={styles.homescreen}>
-          <Text>
-            These miniatures are some of the most glorious manifestations of
-            today Afghanistan’s cultural legacy and also that of Pakistan,
-            Uzbekistan, Tajikistan, India and Iran, hence the need to make them
-            available to the widest possible audience, which will become a
-            reality through the proposed applications.
-          </Text>
-          <Text> ___________________________________________________ </Text>
-        </View>
-        <View style={styles.bottomButton}>
-          <Icon.Button
-            name="chevron-down"
-            size={30}
-            backgroundColor="#ffa500"
-            onPress={() => {}}
-          ></Icon.Button>
-        </View>
+        <ScrollView
+          ref={(view) => {
+            this.scrollView = view;
+          }}
+          showsHorizontalScrollIndicator={false}
+          pagingEnabled
+        >
+          {this.getAllImages()}
+        </ScrollView>
+        {this.scrolledId <= this.state.images.length && 
+          <View style={styles.bottomButton}>
+            <View
+              style={{
+                // width: 40,
+                position: "absolute",
+                bottom: 50,
+                paddingVertical: 0,
+                paddingHorizontal: 0,
+              }}
+            >
+              <Icon.Button
+                name="chevron-down-outline"
+                backgroundColor="#ffa500"
+                paddingLeft={8}
+                paddingRight={0}
+                size={30}
+                onPress={() => {
+                  this.scrollTo();
+                }}
+              ></Icon.Button>
+            </View>
+          </View>
+        }
       </View>
     );
   }
-}
 
+  setViewY = (key, layout) => {
+    this.viewY[key] = layout;
+  };
+  loadImage = (item) => {
+    return (
+      <ScrollView
+        style={styles.homescreen}
+        contentContainerStyle={{
+          alignItems: "center",
+          alignSelf: "center",
+          justifyContent: "center",
+        }}
+        key={item.id}
+        onLayout={(event) => {
+          const layout = event.nativeEvent.layout;
+          this.setViewY(item.id, layout.y);
+        }}
+      >
+        <Image
+          source={item.url}
+          style={{
+            width: Dimensions.get("window").width * 0.99,
+            height: Dimensions.get("window").height * 0.7,
+            borderWidth: 2,
+            borderColor: "white",
+            resizeMode: "cover",
+            marginVertical: Dimensions.get("window").width * 0.1,
+            marginHorizontal: Dimensions.get("window").width * 0.02,
+          }}
+        />
+        <Text>{item.title}</Text>
+        <Text>{item.description}</Text>
+      </ScrollView>
+    );
+  };
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
   homescreen: {
-    paddingTop: 60,
+    paddingTop: 50,
+    paddingLeft: 10,
+    paddingRight: 10,
     backgroundColor: "white",
     marginHorizontal: 5,
   },
